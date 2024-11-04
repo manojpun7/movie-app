e72cd9f992d49ee12553999898bce079318c7290
Manoj <punmanoj2060@gmail.com>
2024-11-04 12:59:36 +0545

    created navbar components

---
+++ src/app/about/page.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        "Welcome to Movie App—your destination for discovering movies and
+        exploring cinematic worlds. Our application is built using Next.js, a
+        modern framework for creating fast, efficient, and responsive web
+        applications. Designed for movie enthusiasts, our app allows you to
+        search for and explore a vast collection of films, drawing on a
+        comprehensive public API for accurate, up-to-date information on
+        thousands of movies. With Movie App, you can: Search for movies by
+        title, genre, release date, and more. Discover popular and trending
+        films. Access detailed information on each movie, including plot
+        summaries, cast and crew, trailers, and reviews. Enjoy a seamless,
+        fast-loading experience on both desktop and mobile. Our mission is to
+        make movie discovery enjoyable and easy. Whether you're looking to find
+        information on classic films or the latest releases, Movie App offers a
+        streamlined experience tailored for cinephiles and casual viewers alike.
+        Thank you for choosing Movie App—happy searching!"
+      </p>
+    </div>
+  );
+}
