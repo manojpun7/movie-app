6f0eed8de8a02e23b8fe06b3e21aac78d91bbcf7
Manoj <punmanoj2060@gmail.com>
2024-11-18 10:35:15 +0545

    width of image

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Loading() {
   return (
     <div className='flex justify-center mt-16'>
-      <Image className='h-55' src="spinner.svg" alt="loading..." />
+      <Image className='h-55' src="spinner.svg" alt="loading..." width={500} height={500} />
     </div>
   )
 }
